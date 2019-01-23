1afcc089447a6288c3b8a4a7beca30097a808fb6
Sagnior <38483140+Sagnior@users.noreply.github.com>
2019-01-23 18:37:04 -0400

    Add files via upload

--- SAUL-UGUETO, ALEJANDRO-GRATEROL.asm
+++ SAUL-UGUETO, ALEJANDRO-GRATEROL.asm
@@ -2,12 +2,18 @@
 #Fecha: 09/02/2019
 
 .data
+	Menu: .asciiz "	SELECCIONE UN NUMERO DE OPCION:\n	1-INSERTAR ERROR\n	2-DETECTAR ERROR\n"
 	Direccion: .asciiz "ENTRADA.txt"
 	buffer: .space 3145728 	# 3MB
 .text
+	#Imprimiendo Menu de opciones
+	la $a0, Menu	#load address del menu a imprimir
+	li $v0, 4
+	syscall
+	
 	# ABRIENDO ARCHIVO
 	li $v0, 13		# 13 = Open File
-	la $a0, Direccion  # load addres, carga la direccion del archivo en $a0
+	la $a0, Direccion  # load address, carga la direccion del archivo en $a0
 	li $a1, 0 	   # modo lectura = 0 , modo escritura= 1,
 	li $a2, 0 	   # modo ignorado para que se pueda hacer la lectura del archivo
 	syscall		   # Listo, archivo abierto para lectura
@@ -23,5 +29,9 @@
 	syscall
 	# $v0 contiene el numero de caracteres leidos , el cual guardaremos en $s2
 	move $s2, $v0   # largo del buffer = $s2 , p=$s2
-	
+
+InsercionDeError:
+	addi $t0, $0, 3 #agregando el valor 3 al registro $t0 
+	div $s2, $t0	#  p/3 
+	mflo $t0	# t0= p/3 (division entera)
 	
